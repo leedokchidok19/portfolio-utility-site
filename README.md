2364a23f4fc87852534c00f80058db226cf91c2e
rom <47617159+leedokchidok19@users.noreply.github.com>
2021-06-06 01:04:58 +0900

    HTML: add select box, en=ko
    select box 추가
    HTML, JS

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # portfolio-utility-site
 utility simple web site
+---
+# Functional Table(기능 목록)
+|Function|Status|
+|---:|:---:|
+|Uppercase|⭕|
+|Lowercase|⭕|
+|Camel case|🗙|
+|Pascal case|🗙|
+|Snake case|🗙|
--- main.js
+++ main.js
@@ -7,17 +7,52 @@ toggleBtn.addEventListener('click', () => {
     icons.classList.toggle('active');
 })
 
+//text transformation select box
+const select_box = document.querySelector('#word__select');
+
+const optionList = [
+                    {key:'대문자', value:1},
+                    {key:'소문자', value:2},
+                    {key:'카멜 표기법', value: 0},
+                    {key:'파스칼 표기법', value: 0},
+                    {key:'스네이크 표기법', value: 0},
+                    ];
+
+for(let i =0; i < optionList.length;i++) {
+    var opions = new Option();
+    opions.value = optionList[i].value;
+    opions.text = optionList[i].key;
+    select_box.add(opions);
+}
+
+select_box.addEventListener('change', (e) => {
+    let index = e.target.options.selectedIndex;
+    let target = e.target.options[index].innerText;
+    let option = e.target.value;
+    console.log(option);
+    if(option === "0") {
+        alert(`${target} 미구현된 기능입니다.`);
+    }
+})
+
 //text
 const word = document.querySelector('#word__transformation');
 
 //doText
-word.addEventListener('keyup', (e) =>{
+word.addEventListener('keyup', (e) => {
     let text = e.target.value;
     doUppercase(text);
+    doLowercase(text);
 })
 
 //text uppercase
 const uppercase = document.querySelector('#word__uppercase');
 function doUppercase(text) {
     return uppercase.value = text;
+}
+
+//text lowercase
+const lowercase = document.querySelector('#word__lowercase');
+function doLowercase(text) {
+    return lowercase.value = text;
 }
