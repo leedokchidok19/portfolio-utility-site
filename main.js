cad70ea06993fde919743ceba1df6055fbbf3d92
rom <47617159+leedokchidok19@users.noreply.github.com>
2021-06-07 00:19:21 +0900

    HTML:snake case
    스네이크 표기법 구현, 주석 추가

--- main.js
+++ main.js
@@ -7,15 +7,16 @@ toggleBtn.addEventListener('click', () => {
     icons.classList.toggle('active');
 })
 
+//.word__title
 //text transformation select box
+/*
 const select_box = document.querySelector('#word__select');
-
 const optionList = [
-                    {key:'대문자', value:1},
-                    {key:'소문자', value:2},
-                    {key:'카멜 표기법', value: 0},
-                    {key:'파스칼 표기법', value: 0},
-                    {key:'스네이크 표기법', value: 0},
+                    {key:'대문자', value:1, id:'word__uppercase'},
+                    {key:'소문자', value:2, id:'word__lowercase'},
+                    {key:'카멜 표기법', value: 0, id:'word__camelcase'},
+                    {key:'파스칼 표기법', value: 0, id:'word__pascalcase'},
+                    {key:'스네이크 표기법', value: 5, id:'word__snakecase'},
                     ];
 
 for(let i =0; i < optionList.length;i++) {
@@ -29,11 +30,30 @@ select_box.addEventListener('change', (e) => {
     let index = e.target.options.selectedIndex;
     let target = e.target.options[index].innerText;
     let option = e.target.value;
-    console.log(option);
+
     if(option === "0") {
         alert(`${target} 미구현된 기능입니다.`);
+        return false;
     }
+
+    let wordId = optionList[index].id;
+    wordForm(wordId);
 })
+*/
+//.word__form-div
+//word form
+function wordForm(wordId) {
+    const word_result = document.querySelector('.word__result');
+    word_result.childNodes[0];
+    const textarea = document.createElement('textarea');
+    textarea.id = wordId;
+    textarea.classList.add('word__form-text');
+    textarea.cols = 50;
+    textarea.rows = 10;
+    textarea.maxLength = 500;
+    
+    word_result.appendChild(textarea);
+}
 
 //text
 const word = document.querySelector('#word__transformation');
@@ -43,6 +63,9 @@ word.addEventListener('keyup', (e) => {
     let text = e.target.value;
     doUppercase(text);
     doLowercase(text);
+    //doCamelcase(text);
+    //doPascalcase(text);
+    doSnakecase(text);
 })
 
 //text uppercase
@@ -55,4 +78,22 @@ function doUppercase(text) {
 const lowercase = document.querySelector('#word__lowercase');
 function doLowercase(text) {
     return lowercase.value = text;
+}
+/*
+//text camelcase
+const camelcase = document.querySelector('#word__camelcase');
+function doCamelcase(text) {
+    return camelcase.value = text;
+}
+
+//text pascalcase
+const pascalcase = document.querySelector('#word__pascalcase');
+function doPascalcase(text) {
+    return pascalcase.value = text;
+}
+*/
+//text snakecase
+const snakecase = document.querySelector('#word__snakecase');
+function doSnakecase(text) {
+    return snakecase.value = text.replaceAll(' ', '_');
 }
